1ee869d82124d40c26b6b0b2fd8bbac3cc01f0b6
harahashinaoya <mogamimaly3603@gmail.com>
2021-01-13 15:28:24 +0900

    5-1 Vue Routerの導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix');
 
 mix.js('resources/js/home.js', 'public/js')
     .js('resources/js/quiz.js', 'public/js')
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css', {
         implementation: require('node-sass')
     });
@@ -10,5 +11,5 @@ mix.browserSync({
     files: [
         "public/**/*.*"
     ],
-    proxy: "http://localhost:8000"
+    proxy: 'http://localhost:8000'
 });
